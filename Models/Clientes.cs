07326b32e6f7933443dc76c839b9b5827e39db50
AnaMarBlanco <Ana_matinez10@ucne.edu.do>
2021-06-30 18:00:10 -0400

    Add project files.

---
+++ Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apli2SegundoParcial.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+
+    
+}
